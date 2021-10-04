701badbba363a26ee162786df3b84709a678f427
camille <camille@localhost>
2021-10-04 11:12:49 +0200

    Depôt exercice 1

--- TP1/src/tp1/TP1.java
+++ TP1/src/tp1/TP1.java
@@ -4,8 +4,11 @@
  */
 package tp1;
 
+import java.util.Scanner;
+
 /**
  *
+ * Gérardin, TD A, Saisie et manipulation de nombres, 04/10/2021
  * @author camille
  */
 public class TP1 {
@@ -15,6 +18,29 @@ public class TP1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        int entier1;
+        System.out.println("Please enter the first int :");
+        entier1=sc.nextInt();
+        int entier2;
+        System.out.println("Please enter the second int :");
+        entier2=sc.nextInt();
+        int somme;
+        somme = entier1 + entier2;
+        int difference;
+        difference = entier1-entier2;
+        int produit; 
+        produit = entier1*entier2;
+        System.out.println("La somme vaut:" + somme + "\n La difference vaut:" + difference + "\n Le produit vaut:" + produit);
+        int quotient; 
+        quotient = entier1/entier2;
+        System.out.println("Le quotient vaut:" + quotient);
+        int reste;
+        reste = entier1 % entier2 ; 
+        System.out.println("Le reste vaut:" + reste);
+        
+                
+        
     }
     
 }
