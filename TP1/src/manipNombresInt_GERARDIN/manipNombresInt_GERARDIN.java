fe1837294c036a9728b9c7f73f90b35c29bca494
camille <camille@localhost>
2021-10-04 12:53:16 +0200

    renommage des répertoires

--- TP1/src/manipNombresInt_GERARDIN/manipNombresInt_GERARDIN.java
+++ TP1/src/manipNombresInt_GERARDIN/manipNombresInt_GERARDIN.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package tp1;
+package manipNombresInt_GERARDIN;
 
 import java.util.Scanner;
 
@@ -11,7 +11,7 @@ import java.util.Scanner;
  * Gérardin, TD A, Saisie et manipulation de nombres, 04/10/2021
  * @author camille
  */
-public class TP1 {
+public class manipNombresInt_GERARDIN {
 
     /**
      * @param args the command line arguments
